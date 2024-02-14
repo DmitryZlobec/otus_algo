fc9ec7597a5a7a29a4cd469e2c7a6a55c607a879
Dmitry Petrenko <zlobec@gmail.com>
2024-02-14 20:20:50 +0300

    Initial commit

---
+++ happy/src/main.rs
@@ -0,0 +1,29 @@
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn happy() -> i32{
+    let mut count =0;
+    for a in 0..10 {
+        for b in 0..10 {
+            for c in 0..10 {
+                for d in 0..10 {
+                    for e in 0..10 {
+                        for f in 0..10  {
+                            if a+b+c == d+e+f {
+                                count = count +1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return count;
+}
+
+fn main() {
+    let start = SystemTime::now();
+    println!("Happy tickets: {}", happy());
+    let finish = SystemTime::now();
+    println!("Time ms: {}", (finish.duration_since(UNIX_EPOCH).expect("error") - start.duration_since(UNIX_EPOCH).expect("error")).as_millis());
+}
