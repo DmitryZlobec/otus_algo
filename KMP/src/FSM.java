53a92780bdaf45328669d617eff5e320e47a3b8b
Dmitry Petrenko <zlobec@gmail.com>
2024-07-13 21:32:11 +0300

    FSM

--- KMP/src/FSM.java
+++ KMP/src/FSM.java
@@ -12,20 +12,35 @@ public class FSM {
     }
 
     private void initDelta(String pattern) {
-        delta = new int[length+1][alphabet.length()];
+        delta = new int[length + 1][alphabet.length()];
 
-            IntStream.range(0, length+1).forEach(q-> {
-                for (byte c: alphabet.getBytes()) {
-                    int k=q+1;
-                    if(q == length) k--;
-                    String line = left(pattern,q)+ (char) c;
-                    while (!left(pattern,k).equals(right(line,k)) ) {
-                        k--;
-                    }
-                    delta[q][c-alphabet.getBytes()[0]] = k;
+        IntStream.range(0, length + 1).forEach(q -> {
+            for (byte c : alphabet.getBytes()) {
+                int k = q + 1;
+                if (q == length) k--;
+                String line = left(pattern, q) + (char) c;
+                while (!left(pattern, k).equals(right(line, k))) {
+                    k--;
                 }
-            });
+                delta[q][c - alphabet.getBytes()[0]] = k;
+            }
+        });
     }
+
+    public int search(String text) {
+        int q = 0;
+        for (int i = 0; i < text.length(); i++) {
+            int aByte = text.getBytes()[i];
+            int aByte1 = alphabet.getBytes()[0];
+            int i1 = aByte - aByte1;
+            q = delta[q][i1];
+            if (q == length) {
+                return i - length + 1;
+            }
+        }
+        return -1;
+    }
+
 
     private static String left(String line, int x) {
         return line.substring(0,x);
--- KMP/src/Main.java
+++ KMP/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         FSM fsm = new FSM("AABAABAAABA");
-        System.out.println("fsm = " + fsm);
+        System.out.println("fsm = " + fsm.search("AAABAABAAABA"));
     }
 }
